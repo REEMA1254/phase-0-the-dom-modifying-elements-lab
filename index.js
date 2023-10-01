46c28ba9383b2c430b4a400e54d66c81ab4517c1
maryam <Reemaali9048@gmail.com>
2023-10-02 00:17:51 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainElement =document.querySelector('#main') 
+ mainElement.remove()
+
+const newHeader =documument.createElement('H1');
+newHeader.setAttribute('victory');
+ 
+newHeader.textContent = "YOUR-NAME is the champion";
